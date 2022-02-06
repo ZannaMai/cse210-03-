2b0f82e54d996917fc62efddf6af12aaf92e885a
ZannaMai <97138655+ZannaMai@users.noreply.github.com>
2022-02-05 19:38:01 -0700

    commit chnages

--- jumper/game/parachute.py
+++ jumper/game/parachute.py
@@ -4,4 +4,8 @@ class Parachute:
     """Where we create and change the tiers of the parachute
     
     """
-    pass
+    def __init__(self):
+        """Constructs the parachute"""
+
+        self._parachutes = 0
+        self._parachute = 0
